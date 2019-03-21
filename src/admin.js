9c974eb4fdbce2bc98576a9e5a324fe4227a07dc
zyx <1113470466@qq.com>
2019-03-21 22:38:43 +0800

    home页面 主内容去样式设置

--- src/admin.js
+++ src/admin.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
-import {Row,Col} from 'antd';
+import { Row, Col } from 'antd';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import NavLeft from './components/NavLeft';
 import './style/common.less';
-export default class Admin extends Component{
-    render(){
+import Home from './pages/home';
+export default class Admin extends Component {
+    render() {
         return (
             <Row className="container">
                 <Col span={3} className="nav-left">
-                   <NavLeft/>
+                    <NavLeft />
                 </Col>
                 <Col span={21} className="main">
-                   <Header/>
-                   <Row className="content">
-                   content
-                   {/* {this.props.children} */}
-                   </Row>
-                   <Footer/>
+                    <Header />
+                    <Row className="content">
+                        <Home />
+                        {/* {this.props.children} */}
+                    </Row>
+                    <Footer />
                 </Col>
             </Row>
         );
