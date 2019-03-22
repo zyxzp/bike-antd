b45d478e4f7f2f039b701f96cecd431a81fa03db
zyx <1113470466@qq.com>
2019-03-22 22:18:36 +0800

    antd 常用UI组件 button modals notification message gallery画廊 carousel轮播图等

--- src/router.js
+++ src/router.js
@@ -4,8 +4,15 @@ import App from './App';
 import Login from './pages/login';
 import NoMatch from './pages/nomatch';
 import Admin from './admin';
-import Buttons from './pages/ui/buttons';
 import Home from './pages/home';
+import Buttons from './pages/ui/buttons';
+import Modals from './pages/ui/modals';
+import Loadings from './pages/ui/loadings';
+import Notices from './pages/ui/notice';
+import Messages from './pages/ui/messages';
+import MyTabs from './pages/ui/tabs';
+import Gallery from './pages/ui/gallery';
+import Carousels from './pages/ui/carousel';
 
 export default class IRouter extends React.Component {
     render() {
@@ -19,6 +26,13 @@ export default class IRouter extends React.Component {
                                 <Switch>
                                     <Route path='/home' component={Home} />
                                     <Route path="/ui/buttons" component={Buttons} />
+                                    <Route path="/ui/modals" component={Modals} />
+                                    <Route path="/ui/loadings" component={Loadings} />
+                                    <Route path="/ui/notification" component={Notices} />
+                                    <Route path="/ui/messages" component={Messages} />
+                                    <Route path="/ui/tabs" component={MyTabs} />
+                                    <Route path="/ui/gallery" component={Gallery} />
+                                    <Route path="/ui/carousel" component={Carousels} />
                                     <Route component={NoMatch} />
                                 </Switch>
                             </Admin>
