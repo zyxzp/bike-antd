cbc71576b3b31ea30a22326772629f18e9bc9302
zyx <1113470466@qq.com>
2019-03-24 22:02:10 +0800

    订单详情 地图展示 1、index.html中引入百度地图api 2、detail.js 地图相关对象要通过window调用         //初始化地图对象         this.map = new window.BMap.Map('orderDetailMap');         // this.map.centerAndZoom('北京', 11);         // 添加地图控件         this.addMapControl();         // 调用路线图绘制方法         this.drawBikeRoute(result.position_list);         // 调用服务区绘制方法         this.drwaServiceArea(result.area);

--- src/pages/order/detail.js
+++ src/pages/order/detail.js
@@ -25,13 +25,89 @@ export default class OrderDetail extends React.Component {
                 this.setState({
                     orderInfo: res.result
                 })
-                // this.renderMap(res.result);
+                this.renderMap(res.result);
             }
         })
     }
+    //绘制地图
+    renderMap = (result) => {
+        this.map = new window.BMap.Map('orderDetailMap');
+        // this.map.centerAndZoom('北京', 11);
+        // 添加地图控件
+        this.addMapControl();
+        // 调用路线图绘制方法
+        this.drawBikeRoute(result.position_list);
+        // 调用服务区绘制方法
+        this.drwaServiceArea(result.area);
+    }
+    // 添加地图控件
+    addMapControl = () => {
+        let map = this.map;
+        map.addControl(new window.BMap.ScaleControl({ anchor: window.BMAP_ANCHOR_TOP_RIGHT }));
+        map.addControl(new window.BMap.NavigationControl({ anchor: window.BMAP_ANCHOR_TOP_RIGHT }));
+    }
+    // 绘制用户的行驶路线
+    drawBikeRoute = (positionList) => {
+        let startPoint = '';
+        let endPoint = '';
+        if (positionList.length > 0) {
+            let first = positionList[0];
+            let last = positionList[positionList.length - 1];
+            startPoint = new window.BMap.Point(first.lon, first.lat);
+            let startIcon = new window.BMap.Icon('/assets/start_point.png', new window.BMap.Size(36, 42), {
+                imageSize: new window.BMap.Size(36, 42),
+                anchor: new window.BMap.Size(18, 42)
+            })
+            //起点
+            let startMarker = new window.BMap.Marker(startPoint, { icon: startIcon });
+            this.map.addOverlay(startMarker);
 
+            endPoint = new window.BMap.Point(last.lon, last.lat);
+            let endIcon = new window.BMap.Icon('/assets/end_point.png', new window.BMap.Size(36, 42), {
+                imageSize: new window.BMap.Size(36, 42),
+                anchor: new window.BMap.Size(18, 42)
+            })
+            //终点
+            let endMarker = new window.BMap.Marker(endPoint, { icon: endIcon });
+            this.map.addOverlay(endMarker);
+
+            // 连接路线图
+            let trackPoint = [];
+            positionList.map((point)=>{
+                trackPoint.push(new window.BMap.Point(point.lon, point.lat));
+            })
+            // for (let i = 0; i < positionList.length; i++) {
+            //     let point = positionList[i];
+            //     trackPoint.push(new window.BMap.Point(point.lon, point.lat));
+            // }
+
+            let polyline = new window.BMap.Polyline(trackPoint, {
+                strokeColor: '#1869AD',
+                strokeWeight: 3,
+                strokeOpacity: 1
+            })
+            this.map.addOverlay(polyline);//折线
+            this.map.centerAndZoom(endPoint, 11);//根据终点定位地图中心
+        }
+    }
+    // 绘制服务区
+    drwaServiceArea = (positionList)=>{
+        // 连接路线图
+        let trackPoint = [];
+        positionList.map((point)=>{
+            trackPoint.push(new window.BMap.Point(point.lon, point.lat));
+        })
+        // 绘制服务区
+        let polygon = new window.BMap.Polygon(trackPoint, {
+            strokeColor: '#CE0000',
+            strokeWeight: 4,
+            strokeOpacity: 1,
+            fillColor: '#ff8605',
+            fillOpacity:0.4
+        })
+        this.map.addOverlay(polygon);
+    }
     render() {
-        // let id = this.props.match.params.id;
         let info = this.state.orderInfo || {};
         return (
             <Card>
